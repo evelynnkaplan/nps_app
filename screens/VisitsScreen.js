1155204c81526bfbd8c13a7ecf1c7c6e06417f1a
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-07-29 16:14:05 -0700

    visit history is displayed

--- screens/VisitsScreen.js
+++ screens/VisitsScreen.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import {Button, StyleSheet, Text, View } from 'react-native';
+import {Button, FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import * as firebase from 'firebase';
 
 class VisitsScreen extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      visitsArray: this.props.navigation.getParam('visitsArray', [])
+    }
+  }
 
   static navigationOptions = ({ navigation }) => {
     return {
@@ -12,11 +19,60 @@ class VisitsScreen extends React.Component {
 
   render() {
 
+    const formatVisits = (array) => {
+      let formattedVisits = [];
+      array.forEach((visit) => {
+        let formattedVisit = {};
+        visit = visit.split(', ');
+        formattedVisit['visitor'] = visit[0];
+        formattedVisit['park'] = visit[1];
+        formattedVisit['date'] = visit[2]
+        formattedVisits.push(formattedVisit);
+      });
+      formattedVisits = formattedVisits.sort((a, b) => b.date.localeCompare(a.date));
+      return formattedVisits;
+    }
+
+    const formattedVisits = formatVisits(this.state.visitsArray);
+
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={styles.container}>
+         <FlatList 
+          data={formattedVisits}
+          renderItem={({item}) => 
+            <TouchableOpacity 
+              style={styles.passData}>
+              <Text>{item.date}</Text>
+              <Text>{item.park}</Text>
+              <Text>{item.visitor}</Text>
+            </TouchableOpacity>}
+          keyExtractor={item => item}
+         />
       </View>
     );
   }
 }
 
 export default VisitsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1D6D3B"
+  },
+  passData: {
+    padding: 10,
+    margin: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white"
+  },
+  header: {
+    color: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 15
+  }
+});
